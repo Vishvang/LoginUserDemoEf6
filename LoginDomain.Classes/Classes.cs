f0168a92d4f7cea5e89695778d5f583d87f3b6ed
Vishvang Patel <vpatel@pinchapenny.com>
2021-03-16 13:12:38 -0400

    Add project files.

---
+++ LoginDomain.Classes/Classes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoginDomain.Classes
+{
+
+    //Can Define more classes
+
+    //POCO - Plain odd class object
+
+    public class User
+    {
+        public Guid UserID { get; set; }
+        public string Username { get; set; }
+        public string LoginName { get; set; }
+        //public string HashedPassword { get; set; }
+        public string Pwd { get; set; }
+
+        /*Unnecessory field*/
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        
+    }
+
+}
