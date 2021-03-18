889621e3868c4f25ea59a0ec639cf4b21e74f8d8
Vishvang Patel <vpatel@pinchapenny.com>
2021-03-17 21:36:10 -0400

    So now after enabling migration I am going to create a migration file so I did. With command add-migration "Initial" will create a migration class.

---
+++ LoginDomain.DataModel/Migrations/202103180133405_Initial.cs
@@ -0,0 +1,47 @@
+﻿namespace LoginDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        RoleName = c.String(),
+                        User_UserID = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.User_UserID, cascadeDelete: true)
+                .Index(t => t.User_UserID);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Guid(nullable: false),
+                        Username = c.String(),
+                        LoginName = c.String(),
+                        Pwd = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        PhoneNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UserRoles", "User_UserID", "dbo.Users");
+            DropIndex("dbo.UserRoles", new[] { "User_UserID" });
+            DropTable("dbo.Users");
+            DropTable("dbo.UserRoles");
+        }
+    }
+}
